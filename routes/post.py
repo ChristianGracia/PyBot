2a37ced5e843c958ba6882826c9d8485297bc027
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-10 00:13:12 -0500

    made post message, post ticker, and post sentiment dynamic

--- routes/post.py
+++ routes/post.py
@@ -1,13 +1,11 @@
 import os
 import json
 import time
-from flask import Blueprint
+from flask import Blueprint, request
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
-
 
 post = Blueprint('post', __name__)
 
@@ -21,47 +19,44 @@ def set_chrome_options() -> None:
 
 @post.route('/st') 
 def post_stocktwits():
+
+    request_body = request.get_json()
+    print(request_body)
+
     driver = webdriver.Chrome(options=set_chrome_options(), executable_path=r"{}".format(os.getenv('CHROME_DRIVER_URL')))  
     driver.get("https://www.stocktwits.com")
     driver.implicitly_wait(5)
 
     # Login
     login_button = driver.find_elements_by_xpath('//*[@id="mainNavigation"]/div[3]/div/div/div[1]/button')[0]
-
     login_button.click()
 
+    # Enter login credentials
     user_input = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[1]/label/input')[0]
     user_input.send_keys(os.getenv('ST_USERNAME'))
     password_input = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[1]/div[2]/label/input')[0]
     password_input.send_keys(os.getenv('ST_PASSWORD'))
-    # password_input.send_keys(Keys.ENTER)
+
+    
     submit_button = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[2]/div[1]/button')[0]
     submit_button.click()
 
     time.sleep(3)
 
-    # Navigate to wanted Stock Symbol
-    ticker = 'SPY'
-    bullish_button = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div[1]')[0]
-    bullish_button.click()
+    message = "${} {}".format(request_body['ticker'], request_body['message'])
 
-    message = "${} Bullish".format(ticker)
+    sentiment_xpath = '//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div[{}]'
+
+    sentiment_button = driver.find_elements_by_xpath(sentiment_xpath.format(1 if request_body['bullish'] else 2))[0]
+    sentiment_button.click()
 
     message_input = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div')[0]
     message_input.click()
     message_input.send_keys(message)
-    # driver.get("https://stocktwits.com/symbol/{}".format(ticker))
-    # time.sleep(5)
-
-    # Post Message
-    # driver.findElement(By.xpath("//*[@id='app']/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]")).click();
-    # post_button = driver.find_elements_by_xpath('//*[@id="mainNavigation"]/div/div[5]/div[1]/span/button')[0]
-    # post_button.click()
-    # message_input = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[4]/div[2]/div/div[2]/div/div[2]/input')[0]
-    # message_input.send_keys(message)
     
     submit_post_button = driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[3]/div[1]/button')[0]
-    submit_post_button.click()
+    # submit_post_button.click()
+
     time.sleep(3)
 
     driver.quit()
