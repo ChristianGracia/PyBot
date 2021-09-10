df387a71c284d316329c5ea50454ab4bb4b048d0
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-09 21:06:55 -0500

    adding current build

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ import os
 from routes.post import post
 from dotenv import load_dotenv
 
-load_dotenv()
+# Get the path to the directory this file is in
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+# Connect the path with your '.env' file name
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
+test_var = os.getenv("TEST_VAR")
 
 app = Flask(__name__)
 app.register_blueprint(post, url_prefix='/post')
--- routes/post.py
+++ routes/post.py
@@ -1,10 +1,9 @@
-from flask import Blueprint
+import os
 import json
-
+from flask import Blueprint
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
-import os
 
 
 post = Blueprint('post', __name__)
@@ -19,9 +18,16 @@ def set_chrome_options() -> None:
 @post.route('/st') 
 def post_stocktwits():
     print(os.getenv('CHROME_DRIVER_URL'))
-    # driver = webdriver.Chrome(options=set_chrome_options(), executable_path=r"{}".format(os.getenv('CHROME_DRIVER_URL')))  
-    # driver.get("http://www.stocktwits.com") 
-    # driver.quit()
+    driver = webdriver.Chrome(options=set_chrome_options(), executable_path=r"{}".format(os.getenv('CHROME_DRIVER_URL')))  
+    driver.get("http://www.stocktwits.com")
+    driver.implicitly_wait(10)
+    # login
+    python_button = driver.find_elements_by_xpath('/html/body/div[3]/div/div/div[2]/div/div/div/div/div[1]/div[1]/div[4]/div/span[2]')[0]
+    driver.implicitly_wait(10)
+    python_button.click()
+    
+
+    driver.quit()
     print("Driver Exited")
 
     return {}
