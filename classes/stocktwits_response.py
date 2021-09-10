4fadbbb74a088ed5b3b2b1274ccb57bfa251c2e7
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-10 02:34:40 -0500

    removed unused imports in class file

--- classes/stocktwits_response.py
+++ classes/stocktwits_response.py
@@ -1,9 +1,3 @@
-import os, time, random
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
 class StockTwitsResponse:
     def __init__(self, ticker, sentiment, message, randomization):
         self.ticker = ticker
